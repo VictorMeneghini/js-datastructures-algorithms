3dcbecaef9dd8875d71c3724539621abef1ec1d8
Victor Meneghini <victormeneghini@pop-os.localdomain>
2023-01-30 19:25:59 +0000

    feat: add finonacci to recursion file

--- src/data-structures/recursion/recursion.ts
+++ src/data-structures/recursion/recursion.ts
@@ -5,3 +5,13 @@ export const factorial = (n: number): number => {
 
   return n * factorial(n - 1)
 }
+
+export const fibonacci = (n: number): any => {
+  if (n < 1) return 0
+  if (n <= 2) return 1
+
+  // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+  return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+console.log(fibonacci(10))
