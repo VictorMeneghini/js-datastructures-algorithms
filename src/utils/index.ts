545f2296359f4d9ec11d25a4d264f599c90a29ee
VictorMeneghini <vhmeneghini@gmail.com>
2023-02-01 19:32:59 +0000

    feat: add new functions to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,10 @@
+export type ICompareFunction<T> = (a: T, b: T) => number
+
 export type IEqualsFunction<T> = (a: T, b: T) => boolean
+
+export type IDiffFunction<T> = (a: T, b: T) => number
+
+export const DOES_NOT_EXIST = -1
 
 export enum Compare {
   LESS_THAN = -1,
@@ -6,7 +12,30 @@ export enum Compare {
   EQUALS = 0
 }
 
-export const defaultToString = (item: any): string => {
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function lesserEquals<T> (a: T, b: T, compareFn: ICompareFunction<T>) {
+  const comp = compareFn(a, b)
+  return comp === Compare.LESS_THAN || comp === Compare.EQUALS
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function biggerEquals<T> (a: T, b: T, compareFn: ICompareFunction<T>) {
+  const comp = compareFn(a, b)
+  return comp === Compare.BIGGER_THAN || comp === Compare.EQUALS
+}
+
+export function defaultCompare<T> (a: T, b: T): number {
+  if (a === b) {
+    return Compare.EQUALS
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN
+}
+
+export function defaultEquals<T> (a: T, b: T): boolean {
+  return a === b
+}
+
+export function defaultToString (item: any): string {
   if (item === null) {
     return 'NULL'
   } else if (item === undefined) {
@@ -15,17 +44,21 @@ export const defaultToString = (item: any): string => {
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
     return `${item}`
   }
-
   return item.toString()
 }
 
-export function defaultEquals<T> (a: T, b: T): boolean {
-  return a === b
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function swap (array: any[], a: number, b: number) {
+  /* const temp = array[a];
+  array[a] = array[b];
+  array[b] = temp; */
+  [array[a], array[b]] = [array[b], array[a]]
 }
 
-export function defaultCompare<T> (a: T, b: T): number {
-  if (a === b) {
-    return Compare.EQUALS
-  }
-  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN
+export function reverseCompare<T> (compareFn: ICompareFunction<T>): ICompareFunction<T> {
+  return (a, b) => compareFn(b, a)
+}
+
+export function defaultDiff<T> (a: T, b: T): number {
+  return Number(a) - Number(b)
 }
